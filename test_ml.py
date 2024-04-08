088c92805afee8d78dd7d7eb03a7ba36e6e76a76
jaredplaisance <49218824+jaredplaisance@users.noreply.github.com>
2024-04-07 20:10:29 -0500

    Unit Test

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,28 @@
 import pytest
-# TODO: add necessary import
+from ml.model import train_model, compute_model_metrics, performance_on_categorical_slice
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+# Test if train_model returns an instance of the RandomForestClassifier
+def test_model_instance():
+    X_train = pd.DataFrame({'feature1': [1, 2, 3], 'feature2': [4, 5, 6]})
+    y_train = pd.Series([0, 1, 0])
+    model = train_model(X_train, y_train)
+    assert isinstance(model, RandomForestClassifier)
 
+# Test if compute_model_metrics returns the expected value
+def test_compute_model_metrics_value():
+    y_true = np.array([0, 1, 0, 1, 0])
+    y_pred = np.array([0, 1, 1, 1, 0])
+    precision, recall, f1 = compute_model_metrics(y_true, y_pred)
+    assert precision == 0.6666666666666666
+    assert recall == 1.0
+    assert f1 == 0.8
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
-
-
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
-    pass
+# Test if the trained model type is RandomForestClassifier
+def test_model_type():
+    X_train = pd.DataFrame({'feature1': [1, 2, 3], 'feature2': [4, 5, 6]})
+    y_train = pd.Series([0, 1, 0])
+    model = train_model(X_train, y_train)
+    assert type(model).__name__ == 'RandomForestClassifier'
